f025039c0a27bd182488d8b4189ed24753ef90bc
cookethan <ethancook48@gmail.com>
2023-12-16 12:29:23 -0600

    updated psudeocode to play game as a best of 5 rounds

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 //  declare variable to hold computers choice
 //  declate variable to hold users choice
+// declare variable to hold users score
+// declare variable to hold computers score
 //  declare variable to hold users choice paragraph in html
 //  declare variable to hold computers choice paragraph in html
 //  declare variable to hold user input field
 //  declare variable to hold user input button
+
 //  create a function to select computers choice
     // generate a random number between 1 and 3
     // write an if else to determine computers choice using the generated number
@@ -14,6 +17,16 @@
     // store users choice in variable
 // create a function to compare the choices and declare a winner
     // compare users choice against computers choice
-    // alter html to display the winner
+    // alter html to display the round winner
 
-// add EventListener to user input button that calls user input function on click
+// create a function that checks if the userScore or computerScore is greater than 3
+    // if score>3 disable input button
+    // declare game winner
+
+// create a function that plays out a round 
+    // call getComputerChoice
+    // call getUserChoice
+    // call compareChoices
+    // call checkScores
+
+// add EventListener to user input button that calls playRound function
