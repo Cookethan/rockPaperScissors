064e241e06a946cbae9307a6317960f489272aa3
cookethan <ethancook48@gmail.com>
2023-12-16 12:11:34 -0600

    Wrote Js psudeocode

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//  declare variable to hold computers choice
+//  declate variable to hold users choice
+//  declare variable to hold users choice paragraph in html
+//  declare variable to hold computers choice paragraph in html
+//  declare variable to hold user input field
+//  declare variable to hold user input button
+//  create a function to select computers choice
+    // generate a random number between 1 and 3
+    // write an if else to determine computers choice using the generated number
+    // store computers choice in variable
+// create a function to get the users input
+    // capture user put
+    // make users input lowercase to avoid variation
+    // store users choice in variable
+// create a function to compare the choices and declare a winner
+    // compare users choice against computers choice
+    // alter html to display the winner
+
+// add EventListener to user input button that calls user input function on click
